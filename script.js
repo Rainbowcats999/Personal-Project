49a2f30749db089e0e6d2bb79a79ffe62b516049
Rainbowcats999 <526erizov@frhsd.com>
2025-03-27 16:24:57 +0000

    Why isn't it working :((((

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
+playerImage.src = 'imgs/shadow_dog.png';
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(50,50,100,100);
-    ctx.drawImage(playerImage, 0, 0);
+   // ctx.fillRect(50,50,100,100);
+   //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, sx, sy, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     requestAnimationFrame(animate);
 };
 animate();
