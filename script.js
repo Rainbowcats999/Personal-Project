169c4c1845ed69426d1e9ce95517fdae66c65314
Rainbowcats999 <526erizov@frhsd.com>
2025-03-27 16:33:20 +0000

    Ok I'll stop here, video was at 14:15

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'imgs/shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
    // ctx.fillRect(50,50,100,100);
    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, 0, 0, 1800, 1800, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(playerImage, 0 * spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     requestAnimationFrame(animate);
 };
 animate();
