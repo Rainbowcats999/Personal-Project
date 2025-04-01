21b45e020c2f7234055624bd34b659b976889970
Rainbowcats999 <526erizov@frhsd.com>
2025-04-01 16:24:31 +0000

    Where'd the dog go :(

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 spriteAnimations = [
     "idle" = {
         loc: [
-            {x:0, y:0},
-            {x: 575, y:0},
-            {x:1150, y:0},
-            {x:1725, y:0},
-            {x:2300, y:0},
-            {x:2875, y:0}
+            { x: 0, y: 0 },
+            { x: 575, y: 0 },
+            { x: 1150, y: 0 },
+            { x: 1725, y: 0 },
+            { x: 2300, y: 0 },
+            { x: 2875, y: 0 }
         ]
     },
     "jump" = {
@@ -16,3 +16,6 @@ spriteAnimations = [
         loc: []
     }
 ];
+
+console.log(spriteAnimations["idle"].loc[2].x); //1150;
+console.log(spriteAnimations["idle"].loc.length); //7
