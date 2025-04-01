b00291ea8876572c494ea1aa90784a22cb56b607
Rainbowcats999 <526erizov@frhsd.com>
2025-04-01 17:44:55 +0000

    Let's go back to the html

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const playerImage = new Image();
 playerImage.src = 'imgs/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
+let playerState = 'fall';
 
 let gameFrame = 0;
 const staggerFrames = 5;
@@ -19,6 +20,38 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
     }
 ];
 animationStates.forEach((state, index) => {
@@ -37,11 +70,11 @@ console.log(animationStates);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].loc.length;
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
     let frameX = spriteWidth * position;
-    let frameY = spriteAnimations["idle"].loc[position].y;
+    let frameY = spriteAnimations[playerState].loc[position].y;
 
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
     gameFrame++;
     requestAnimationFrame(animate);
