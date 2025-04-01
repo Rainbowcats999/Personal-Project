272709c93801d06eb56bc178eacb58f2fbeaad43
Rainbowcats999 <526erizov@frhsd.com>
2025-04-01 14:05:30 +0000

    FrameX and FrameY???

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const playerImage = new Image();
 playerImage.src = 'imgs/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-   // ctx.fillRect(50,50,100,100);
+   //ctx.fillRect(50,50,100,100);
    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, 0 * spriteWidth, 0 * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     requestAnimationFrame(animate);
 };
 animate();
